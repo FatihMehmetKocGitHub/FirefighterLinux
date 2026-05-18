0739f0c5a5d1bcf84e8b4f0f8f150af1c7816909
Fatih Mehmet Koç <fatihmehmet_koc@outlook.com>
2026-05-18 07:49:02 +0000

    Improve branding installer for live systems

--- scripts/install-branding.sh
+++ scripts/install-branding.sh
@@ -2,15 +2,22 @@
 
 echo "Installing Firefighter Linux branding..."
 
+REPO_DIR="$HOME/FirefighterLinux"
+
 # os-release
-sudo cp ~/FirefighterLinux/branding/os-release/firefighter-os-release /etc/os-release
+sudo cp "$REPO_DIR/branding/os-release/firefighter-os-release" /etc/os-release
 
 # GRUB
-sudo cp ~/FirefighterLinux/branding/grub/grub.conf /etc/default/grub
-sudo update-grub
+sudo cp "$REPO_DIR/branding/grub/grub.conf" /etc/default/grub
+
+if mount | grep -q "overlay"; then
+    echo "Live/overlay system detected. Skipping update-grub and update-initramfs."
+else
+    sudo update-grub
+fi
 
 # Plymouth
-sudo cp -r ~/FirefighterLinux/branding/plymouth/firefighterlinux /usr/share/plymouth/themes/
+sudo cp -r "$REPO_DIR/branding/plymouth/firefighterlinux" /usr/share/plymouth/themes/
 
 sudo update-alternatives --install \
 /usr/share/plymouth/themes/default.plymouth default.plymouth \
@@ -19,16 +26,17 @@ sudo update-alternatives --install \
 sudo update-alternatives --set default.plymouth \
 /usr/share/plymouth/themes/firefighterlinux/firefighterlinux.plymouth
 
-sudo update-initramfs -u
+if mount | grep -q "overlay"; then
+    echo "Live/overlay system detected. Skipping update-initramfs."
+else
+    sudo update-initramfs -u
+fi
 
 # Wallpaper
 sudo mkdir -p /usr/share/backgrounds/firefighterlinux
-
-sudo cp ~/FirefighterLinux/branding/wallpapers/* \
-/usr/share/backgrounds/firefighterlinux/
+sudo cp "$REPO_DIR"/branding/wallpapers/* /usr/share/backgrounds/firefighterlinux/
 
 # LightDM
-sudo cp ~/FirefighterLinux/branding/lightdm/lightdm-gtk-greeter.conf \
-/etc/lightdm/
+sudo cp "$REPO_DIR/branding/lightdm/lightdm-gtk-greeter.conf" /etc/lightdm/
 
 echo "Firefighter Linux branding installed."
